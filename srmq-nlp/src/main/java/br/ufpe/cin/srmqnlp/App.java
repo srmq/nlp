a82bbeac063dcc6f8e6353b8b451739a83ef49e1
srmq <srmq@cin.ufpe.br>
2014-11-13 19:34:12 -0300

    Add references to CW embeddings

--- srmq-nlp/src/main/java/br/ufpe/cin/srmqnlp/App.java
+++ srmq-nlp/src/main/java/br/ufpe/cin/srmqnlp/App.java
@@ -6,8 +6,13 @@ package br.ufpe.cin.srmqnlp;
  */
 public class App 
 {
+	public static final String CW_EMBEDDINGS = "C:/Users/Sergio/Dropbox/CIn/research/textmining/devel/senna/embeddings/embeddings.txt";
+	public static final String CW_WORDS = "C:/Users/Sergio/Dropbox/CIn/research/textmining/devel/senna/hash/words.lst";
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+    
+    
 }
